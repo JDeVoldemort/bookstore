6d6b79c0c55cc9df568d916b821d47048d84831f
JDeVoldemort <104032188+JDeVoldemort@users.noreply.github.com>
2023-05-24 19:41:10 -0600

    added code to app.js

--- app.js
+++ app.js
@@ -52,6 +52,10 @@ app.use((req, res, next) => {
 
 .use('/', require( './routes'));
 
+process.on('uncaughtException', (err, origin) => {
+  console.log(process.stderr.fd, `Caught exception: ${err}\n` + `Exception origin: ${origin}`);
+});
+
 mongodb.initDb((err, mongodb) => {
     if (err) {
       console.log(err);
