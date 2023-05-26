88bac84b8428d90ec50283c8a2277bd6fe70205d
JDeVoldemort <dragonlore8@gmail.com>
2023-05-26 15:07:53 -0600

    drop if in fetallstock

--- controllers/stock.js
+++ controllers/stock.js
@@ -5,9 +5,9 @@ const getAllStock = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('stock').find();
  
   result.toArray().then((lists) => {
-    if (err) {
-      res.status(400).json({ message: err });
-    }
+    // if (err) {
+    //   res.status(400).json({ message: err });
+    // }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
