5ec8100811afd9bb8e189c9488d01ca9b456c190
JDeVoldemort <dragonlore8@gmail.com>
2023-05-26 14:59:31 -0600

    bacpeddle

--- controllers/stock.js
+++ controllers/stock.js
@@ -3,11 +3,11 @@ const ObjectId = require('mongodb').ObjectId;
 
 const getAllStock = async (req, res, next) => {
   const result = await mongodb.getDb().db().collection('stock').find();
-  if (err) {
-    res.status(400).json({ message: err });
-  }
+ 
   result.toArray().then((lists) => {
-
+    if (err) {
+      res.status(400).json({ message: err });
+    }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists);
   });
