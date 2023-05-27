7064f9dee949a3025396b6621c119eae0746d73f
JDeVoldemort <dragonlore8@gmail.com>
2023-05-26 22:11:44 -0600

    adjuat get on aq

--- controllers/toAquisition.js
+++ controllers/toAquisition.js
@@ -31,9 +31,9 @@ const getOneAquisition = async (req, res, next) => {
     .collection('toAquisition')
     .find({ _id: userId });
   result.toArray().then((err, lists) => {
-    if (err) {
-      res.status(400).json({ message: err });
-    }
+    // if (err) {
+    //   res.status(400).json({ message: err });
+    // }
     res.setHeader('Content-Type', 'application/json');
     res.status(200).json(lists[0]);
   });
