1952cadca69206dc90f09abc8b7b94387ecd3478
JDeVoldemort <104032188+JDeVoldemort@users.noreply.github.com>
2023-06-03 20:46:15 -0600

    Working on adding a ui. need info on node mvc ui

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,5 @@
 displayName = (req, res) => {
-    let data = 'bookstore before frontend installed';
+    let data = 'Bookstore api access working. Still need a ui';
     res.status(200).send(data);
 };
 
